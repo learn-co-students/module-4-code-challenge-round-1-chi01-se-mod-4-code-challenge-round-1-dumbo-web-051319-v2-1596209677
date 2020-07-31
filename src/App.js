557485785831be994c6f456d5db631795823743b
jayastronomic <juliansmith94@hotmail.com>
2020-07-31 12:02:58 -0500

    lol

--- src/App.js
+++ src/App.js
@@ -3,13 +3,57 @@ import logo from "./logo.svg";
 import "./App.css";
 import BookList from "./containers/BookList";
 import Bookshelf from "./containers/Bookshelf";
+import Form from "./components/Form"
+
+
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      books: [],
+      booksOnShelf: []
+    }
+
+  }
+
+async componentDidMount() {
+  const url = "http://localhost:3005/books"
+  const resp = await fetch(url);
+  const bookData = await resp.json();
+  this.setState( { books: bookData  } )
+}
+
+addToShelf = (id) => {
+  const bookShelf = this.state.booksOnShelf
+  const updatedBooksOnShelf = this.state.booksOnShelf.map(book =>{
+    if (book.id === id ){
+      this.setState({
+        booksOnShelf: [...bookShelf, book]
+      })
+    }
+  })
+}
+
+
+renderNewBook = (book) => {
+    const arr = this.state.books
+    this.setState({
+      books: [...arr, book ]
+    })
+  }
+
+
+  
   render() {
+
+    const books = this.state.books
+    const booksOnShelf = this.state.booksOnShelf
     return (
       <div className="book-container">
-        <BookList />
-        <Bookshelf />
+        <Form newBook={this.renderNewBook} />
+        <BookList addBooks={this.addToShelf} books={books} />
+        <Bookshelf books={booksOnShelf} />
       </div>
     );
   }
--- src/components/Book.js
+++ src/components/Book.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Book = props => {
   return (
-    <div>
-      <h2>{/*book title*/}</h2>
-      {/*book img*/}
+    <div onClick={() => props.addBooks(props.bookData.id) }> 
+      <h2>{props.bookData.title}</h2>
+      <img src={props.bookData.img} ></img>
     </div>
   );
 };
