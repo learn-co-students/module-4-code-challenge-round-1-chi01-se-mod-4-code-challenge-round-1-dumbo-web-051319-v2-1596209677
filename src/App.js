7c86ede372bac996f3e7d65a9e3b206c511fd511
github username <github email address>
2020-07-31 12:01:53 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -1,16 +1,68 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import BookList from "./containers/BookList";
 import Bookshelf from "./containers/Bookshelf";
 
 class App extends Component {
+
+  state = {
+  	books: [],
+  	bookShelf: [],
+  	lastBookId: 0
+  }
+
+  componentDidMount() {
+
+  	fetch('http://localhost:3005/books')
+  	.then(resp => resp.json())
+  	.then(books => {
+  	  	this.setState({books})
+  	  })
+  }
+
+  handleBookClick = (book) => {
+  	if(!this.state.bookShelf.includes(book)) {
+  	  this.setState({
+  		  bookShelf: [...this.state.bookShelf, book]
+  	  })
+  	 }
+  }
+
+  //create a copy of state that can be filtered through to make sure
+  //book id is new for bookshelf
+  handleBookShelfClick = (bookObj) => {
+  	const copyOfState = [...this.state.bookShelf]
+  	const newState = copyOfState.filter(book => book.id !== bookObj.id)
+  	this.setState({
+  		bookShelf: newState
+  	})
+  }
+
+  handleFormSubmit = (e, bookObj) => {
+  	e.preventDefault()
+	if(!this.state.books.includes(bookObj)) {
+  	  this.setState({
+  		books: [bookObj, ...this.state.books]
+  	  })
+  	}
+  }
+ 
   render() {
     return (
-      <div className="book-container">
-        <BookList />
-        <Bookshelf />
-      </div>
+    	<main>
+	      <div className="book-container">
+	        <BookList 
+            handleFormSubmit={this.handleFormSubmit} 
+            handleBookClick={this.handleBookClick} 
+            books={this.state.books} 
+          />
+	        <Bookshelf 
+            handleBookShelfClick={this.handleBookShelfClick} 
+            books={this.state.bookShelf} 
+          />
+	      </div>
+	    </main>
     );
   }
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,50 @@
 import React from "react";
 
 class Form extends React.Component {
-  render() {
-    return <h1>{/*create form*/}</h1>;
+
+  state= {
+    title: '',
+    author: '',
+    img: ''
   }
+
+  handleAddForm = (event) => {
+    event.preventDefault()
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+
+  render() {
+    return ( 
+      <div>
+        <h1>Add a book</h1>
+        <form onSubmit={(event) => this.props.handleFormSubmit(event, this.state)}>
+          <input 
+            onChange={this.handleAddForm} 
+            type='text' 
+            name='title' 
+            placeholder='title' 
+          />
+          <input 
+            onChange={this.handleAddForm} 
+            type='text' 
+            name='author' 
+            placeholder='author' 
+          />
+          <input 
+            onChange={this.handleAddForm} 
+            type='text' 
+            name='img' 
+            placeholder='img' 
+          />
+          <input type='submit' />
+        </form>
+      </div>
+    )
+  }
+
 }
 
 export default Form;
